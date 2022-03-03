a487532751530764904b5d11fe76a1bea276c0f6
Ross MacArthur <ross@macarthur.io>
2022-03-03 17:05:53 +0200

    Use `Cow<str>` in `format_ago` function

--- src/human.rs
+++ src/human.rs
@@ -1,32 +1,34 @@
 use std::time::Duration;
 
-pub fn format_ago(d: Duration) -> String {
+use dairy::Cow;
+
+pub fn format_ago(d: Duration) -> Cow<'static, str> {
     match d.as_secs() {
-        x if x <= 44 => "a few seconds ago".to_string(),
-        x if x <= 89 => "a minute ago".to_string(),
+        x if x <= 44 => "a few seconds ago".into(),
+        x if x <= 89 => "a minute ago".into(),
         x if x <= 44 * 60 => {
             let m = x as f32 / 60.;
-            format!("{:.0} minutes ago", m.ceil())
+            format!("{:.0} minutes ago", m.ceil()).into()
         }
-        x if x <= 89 * 60 => "an hour ago".to_string(),
+        x if x <= 89 * 60 => "an hour ago".into(),
         x if x <= 21 * 60 * 60 => {
             let h = x as f32 / 60. / 60.;
-            format!("{:.0} hours ago", h.ceil())
+            format!("{:.0} hours ago", h.ceil()).into()
         }
-        x if x <= 35 * 60 * 60 => "a day ago".to_string(),
+        x if x <= 35 * 60 * 60 => "a day ago".into(),
         x if x <= 25 * 24 * 60 * 60 => {
             let d = x as f32 / 24. / 60. / 60.;
-            format!("{:.0} days ago", d.ceil())
+            format!("{:.0} days ago", d.ceil()).into()
         }
-        x if x <= 45 * 24 * 60 * 60 => "a month ago".to_string(),
+        x if x <= 45 * 24 * 60 * 60 => "a month ago".into(),
         x if x <= 10 * 30 * 24 * 60 * 60 => {
             let m = x as f32 / 30. / 24. / 60. / 60.;
-            format!("{:.0} months ago", m)
+            format!("{:.0} months ago", m).into()
         }
-        x if x <= 17 * 30 * 24 * 60 * 60 => "a year ago".to_string(),
+        x if x <= 17 * 30 * 24 * 60 * 60 => "a year ago".into(),
         _ => {
             let y = d.as_secs_f32() / 12. / 30. / 24. / 60. / 60.;
-            format!("{:.0} years ago", y)
+            format!("{:.0} years ago", y).into()
         }
     }
 }
